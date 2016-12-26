e389c31c829727c041bc66b7c3dff1c6f8fb2896
VasyaPhilis <urologept@gmail.com>
2016-12-27 01:26:41 +0200

    Adaptive
    Some adaptive rules. Romanu pryvit

--- src/scripts/js/main-script.js
+++ src/scripts/js/main-script.js
@@ -1,87 +1,77 @@
 'use strict';
-
 var main = function () {
     var r = pageId;
-    
-    $('header .cube').css({
-        'transform': 'rotate3d(0, 1, 0, ' + 90 * r + 'deg)'
-    });
-    
+    if (window.matchMedia('(max-width: 320px)').matches) {
+        // don't do functionality on screens smaller than 320px
+        $('header .cube').css({
+            'transform': 'rotate3d(0, 1, 0, ' + 90 * r + 'deg)'
+        });
+    }
     $('.lang-menu').click(function () {
         $('.lang-menu .languages').fadeToggle(300, 'linear');
     });
-    
     $('.lang-menu').mouseleave(function () {
         $('.lang-menu .languages').fadeOut(300, 'linear');
     });
-    
     $('.service .text, .service .button').click(function () {
         $(this).siblings('.demo').fadeIn(300, 'linear');
         $(this).siblings('.demo').css('display', 'flex');
     });
-    
     $('header .arrow:last-of-type').click(function () {
         r++;
         $('header .cube').css({
             'transform': 'rotate3d(0, 1, 0, ' + 90 * r + 'deg)'
         });
     });
-    
     $('header .arrow:first-of-type').click(function () {
         r--;
         $('header .cube').css({
-            'animation-name': 'none',
-            'transform': 'rotate3d(0, 1, 0, ' + 90 * r + 'deg)'
+            'animation-name': 'none'
+            , 'transform': 'rotate3d(0, 1, 0, ' + 90 * r + 'deg)'
         });
     });
-    
     $('.service').click(function () {
         $('.details').css({
-            "right": "0",
-            "opacity": "0",
-            "z-index": "-5"
+            "right": "0"
+            , "opacity": "0"
+            , "z-index": "-5"
         });
         if ($(this).children('.details').css('right') !== "0px") {
             $(this).children('.details').css({
-                "right": "0px",
-                "opacity": "0",
-                "z-index": "-5"
+                "right": "0px"
+                , "opacity": "0"
+                , "z-index": "-5"
             });
-        } else {
+        }
+        else {
             $(this).children('.details').css({
-                "right": "100%",
-                "opacity": "1",
-                "z-index": "1"
+                "right": "100%"
+                , "opacity": "1"
+                , "z-index": "1"
             });
         }
     });
-    
     $('.list').mouseleave(function () {
         $('.details').css({
-            "right": "0px",
-            "opacity": "0",
-            "z-index": "-5"
-        }); 
+            "right": "0px"
+            , "opacity": "0"
+            , "z-index": "-5"
+        });
     });
-        
     $(".sites li").click(function () {
         var target = $(this).attr('class').trim();
         $(".catalog").children('.' + target).slideToggle(300);
         $(".catalog").children(':not(.' + target + ')').slideUp(300);
         $(".catalog").children('.' + target).accordion({
-            collapsible: true,
-            heightStyle: 'content'
+            collapsible: true
+            , heightStyle: 'content'
         });
-        
     });
-    
     $(".sites li").mouseenter(function () {
         $(this).children('p').fadeIn(0);
     });
-    
     $(".sites li").mouseleave(function () {
         $(this).children('p').fadeOut(0);
     });
 };
-
 $(document).ready(main);
